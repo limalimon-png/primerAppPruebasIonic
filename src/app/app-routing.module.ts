034ec37d10bf28fcfb985020314a05f88388e8b7
guillermo <gmartinez@sdi.es>
2022-01-13 09:43:19 +0100

    botones antes de centralizar el back

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+/***IMPORTANTE******
+ * em el app routing module se pone la informacion de la ruta principal de la aplicacion.
+*/
 const routes: Routes = [
-  {
+ /* {
     path: 'home',
+    //lazy load
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+  },*/
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'inicio',
     pathMatch: 'full'
+  },
+  //automaticamente al generar una pagina se nos crea el lazy load
+  {
+    path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'alert',
+    loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule)
+  },
+  {
+    path: 'action-sheet',
+    loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
   },
 ];
 
